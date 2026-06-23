2e03f8af2e6727032639109718a5469f9c8bc27b
Babita <boby240907@gmail.com>
2026-06-23 12:26:25 +0000

    conversion of data types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 //" " => false
@@ -23,5 +23,33 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// *************Operations**************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " harish"
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
