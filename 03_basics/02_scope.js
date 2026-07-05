08036d480ac1cdd60964e81a1d0101df2260be0e
Babita <boby240907@gmail.com>
2026-07-05 10:17:40 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+let a = 400
+
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c); //isko iske scope ke bahr v access kr sakte h isliye var ka use nahi karte
+
+
