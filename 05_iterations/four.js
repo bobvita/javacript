463582ac1f4520ba7f9d77b031c28c386e36733c
Babita <boby240907@gmail.com>
2026-07-13 06:27:16 +0000

    for of, for in, forEach

---
+++ 05_iterations/four.js
@@ -0,0 +1,25 @@
+//for in
+
+const myObject ={
+    js: 'javacript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by app"
+}
+
+for (const key in myObject) {
+    //console.log(myObject[key]); //prints values
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(key); //arr ka index print kar deta hai
+    //console.log(programming[key]);
+    
+} 
+
+//maps me for in loop nhi laga sakte
+
